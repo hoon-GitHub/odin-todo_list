6cd51293d180b7a097e09f6e757cf5d8dd900f0d
unknown <hoon81@gmail.com>
2024-05-09 15:59:19 -0400

    Add delete button and functionality
    Button is added to the edit dialog.

--- src/modules/dialog.js
+++ src/modules/dialog.js
@@ -5,6 +5,7 @@ import { renderProjects } from "./render_nav.js";
 
 // get the DOM elements in the dialog
 const dialog = document.querySelector('dialog');
+const dialogBtnDiv = document.getElementById('dialogBtnDiv');
 const titleInput = document.getElementById('titleInput');
 const descInput = document.getElementById('descInput');
 const dueInput = document.getElementById('dueInput');
@@ -16,14 +17,17 @@ const confirmBtn = document.getElementById('confirmBtn');
 export function renderAddDialog() {
   titleInput.value = "";
   descInput.value = "";
-  dueInput.value = "";
-  priorityInput.value = "med";
+  dueInput.value = format(new Date(), "MM/dd/yyyy"); // default: today
+  priorityInput.value = "med"; // default: med
   if (currentView == "all" || currentView == "date") {
     projectInput.value = "";
   } else {
     projectInput.value = currentView;
   }
   confirmBtn.innerText = "Add";
+
+  // delete button is not applicable here!
+  if (document.getElementById('deleteBtn')) document.getElementById('deleteBtn').remove();
 
   // process the add
   confirmBtn.onclick = (e) => {
@@ -55,6 +59,40 @@ export function renderEditDialog(item) {
   priorityInput.value = item.priority;
   projectInput.value = item.project;
   confirmBtn.innerText = "Save";
+
+  // add a delete button, if not there already
+  if (!document.getElementById('deleteBtn')) {
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('dialogBtn');
+    deleteBtn.id = 'deleteBtn';
+    deleteBtn.innerText = "Delete";
+    dialogBtnDiv.appendChild(deleteBtn);
+  }
+  // delete the item when clicked
+  deleteBtn.onclick = (e) => {
+    e.preventDefault();
+    let index = null;
+
+    // find the index using title and remove item
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === item) {
+        index = i;
+        console.log(i);
+        list.splice(i, 1);
+      }
+    }
+
+    // reload the #content
+    if (currentView == "all" || currentView == "date") {
+      renderAll(list);
+    } else {
+      renderProject(list, currentView);
+    }
+
+    // reload the project list in #nav
+    renderProjects();
+    dialog.close();
+  }
 
   // save the edit
   confirmBtn.onclick = (e) => {
